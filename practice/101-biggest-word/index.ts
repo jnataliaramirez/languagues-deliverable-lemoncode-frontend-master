5dda98073f7e2dfd48626f5976eec3d3b4d1ae68
Natalia Ramirez Parra <jnataliaramirez@outlook.com>
2023-05-23 21:12:14 +0200

    add execute biggest exercise

--- practice/101-biggest-word/index.ts
+++ practice/101-biggest-word/index.ts
@@ -6,3 +6,5 @@ const biggestWord: BiggestWordFunction = (phrase) =>
     .reduce((longest, current) =>
       current.length > longest.length ? current : longest
     );
+
+console.log(biggestWord("Esta frase puede contener muchas palabras")); // "contener"
