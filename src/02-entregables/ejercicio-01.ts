d05972ce43892c43a6093c34e4d8cb2f30076d5b
Natalia Ramirez Parra <jnataliaramirez@outlook.com>
2023-05-29 16:32:39 +0200

    Do first exercise deliverable

--- src/02-entregables/ejercicio-01.ts
+++ src/02-entregables/ejercicio-01.ts
@@ -1,1 +1,25 @@
 console.log("************** DELIVERABLE 01 *********************");
+
+const example = ["lemoncode", 2, true, 4, "nati"];
+
+// * Función Head
+const head = (array) => {
+  const [first] = array;
+  return first;
+};
+// console.log("head()", head(example));
+
+// * Función Tail
+const tail = (array) => {
+  const [_first, ...others] = array;
+  return others;
+};
+// console.log("tail()", tail(example));
+
+// * Función Init
+const init = (array) => array.slice(0, array.length - 1);
+// console.log("init()", init(example));
+
+// * Función Last
+const last = (array) => array.findLast((last) => last);
+// console.log("last()", last(example));
--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@
 
 // *** Entregables
 // import "./02-entregables/ejercicio-01.js";
-// import "./02-entregables/ejercicio-02.js";
+import "./02-entregables/ejercicio-02.js";
 // import "./02-entregables/ejercicio-03.js";
 // import "./02-entregables/ejercicio-04.js";
 // import "./02-entregables/ejercicio-05.js";
