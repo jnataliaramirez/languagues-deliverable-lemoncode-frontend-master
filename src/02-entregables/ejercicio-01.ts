3eddaca13b087ffce7c47275d9c656550b1f21fd
Natalia Ramirez Parra <jnataliaramirez@outlook.com>
2023-05-29 17:44:31 +0200

    Do types first exercise deliverable

--- src/02-entregables/ejercicio-01.ts
+++ src/02-entregables/ejercicio-01.ts
@@ -1,25 +1,30 @@
 console.log("************** DELIVERABLE 01 *********************");
 
-const example = ["lemoncode", 2, true, 4, "nati"];
+// --- Types
+type functionReturnArray = <T>(array: T[]) => T[];
+type functionReturnT = <T>(array: T[]) => T;
+
+// ---
+const example: number[] = [1, 2, 3, 4, 5];
 
 // * Función Head
-const head = (array) => {
+const head: functionReturnT = (array) => {
   const [first] = array;
   return first;
 };
 // console.log("head()", head(example));
 
 // * Función Tail
-const tail = (array) => {
+const tail: functionReturnArray = (array) => {
   const [_first, ...others] = array;
   return others;
 };
 // console.log("tail()", tail(example));
 
 // * Función Init
-const init = (array) => array.slice(0, array.length - 1);
+const init: functionReturnArray = (array) => array.slice(0, array.length - 1);
 // console.log("init()", init(example));
 
 // * Función Last
-const last = (array) => array.findLast((last) => last);
+const last: functionReturnT = (array) => array.findLast((last) => last);
 // console.log("last()", last(example));
--- src/index.ts
+++ src/index.ts
@@ -10,8 +10,8 @@
 // import "./01-practica/114-values/index.js";
 
 // *** Entregables
-// import "./02-entregables/ejercicio-01.js";
-import "./02-entregables/ejercicio-02.js";
+import "./02-entregables/ejercicio-01.js";
+// import "./02-entregables/ejercicio-02.js";
 // import "./02-entregables/ejercicio-03.js";
 // import "./02-entregables/ejercicio-04.js";
 // import "./02-entregables/ejercicio-05.js";
