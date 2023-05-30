30890cc391d5037b4a3e11e81f3593de33cc9ce6
Natalia Ramirez Parra <jnataliaramirez@outlook.com>
2023-05-30 14:25:15 +0200

    Do fourth exercise deliverable

--- src/02-entregables/ejercicio-04.ts
+++ src/02-entregables/ejercicio-04.ts
@@ -1,1 +1,28 @@
 console.log("************** DELIVERABLE 04 *********************");
+
+// --- Types
+
+interface Books {
+  title: string;
+  isRead: boolean;
+}
+
+interface IsBookRead {
+  (books: Books[], titleToSearch: string): boolean;
+}
+
+// ---
+const books: Books[] = [
+  { title: "Harry Potter y la piedra filosofal", isRead: true },
+  { title: "Canción de hielo y fuego", isRead: false },
+  { title: "Devastación", isRead: true },
+];
+
+// * Función isBookRead
+
+const isBookRead: IsBookRead = (books, titleToSearch) =>
+  books.some((item) => (item.title === titleToSearch ? item.isRead : false));
+
+console.log(isBookRead(books, "Devastación")); // true
+console.log(isBookRead(books, "Canción de hielo y fuego")); // false
+console.log(isBookRead(books, "Los Pilares de la Tierra")); // false
--- src/index.ts
+++ src/index.ts
@@ -12,8 +12,8 @@
 // *** Entregables
 // import "./02-entregables/ejercicio-01.js";
 // import "./02-entregables/ejercicio-02.js";
-import "./02-entregables/ejercicio-03.js";
-// import "./02-entregables/ejercicio-04.js";
+// import "./02-entregables/ejercicio-03.js";
+import "./02-entregables/ejercicio-04.js";
 // import "./02-entregables/ejercicio-05.js";
 
 // *** Retos
