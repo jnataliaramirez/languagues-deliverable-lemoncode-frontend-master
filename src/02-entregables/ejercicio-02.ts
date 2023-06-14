fe47d202db850b873c8672a8e4a9ceb036db1db2
Natalia Ramirez Parra <jnataliaramirez@outlook.com>
2023-06-14 05:53:00 +0200

    Add information comments

--- src/02-entregables/ejercicio-02.ts
+++ src/02-entregables/ejercicio-02.ts
@@ -1,6 +1,6 @@
 console.log("************** DELIVERABLE 02 *********************");
 
-// ---
+// --- Datos
 const example1: number[] = [1, 2, 3, 4];
 const example2: string[] = ["lorem", "ipsum", "amet"];
 const example3: number[] = [5, 6, 7, 8];
@@ -10,6 +10,7 @@ const example4: string[] = ["urna", "metus", "blandit"];
 const concat = <T, U>(arr1: T[], arr2: U[]): (T | U)[] => {
   return [...arr1, ...arr2];
 };
+
 console.log("concat()", concat(example1, example2));
 
 //  * -->  Opcional
@@ -17,7 +18,6 @@ type ConcatOpt = <T extends number | string>(...arrays: T[][]) => T[];
 
 const concatOpt: ConcatOpt = (...arrays) => arrays.flat(1);
 
-// * --> Consola
 console.log(
   "concatOpt()",
   concatOpt<number | string>(example2, example1, example4, example3)
