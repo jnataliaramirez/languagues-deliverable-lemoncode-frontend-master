ca76da6bf4dfe440f57f8320e3cb10de8e3e35f7
Natalia Ramirez Parra <jnataliaramirez@outlook.com>
2023-08-09 21:09:30 +0200

    Do 111-reverse exercise

--- src/01-practica/112-subsets/index.ts
+++ src/01-practica/112-subsets/index.ts
@@ -1,9 +1,25 @@
-const subsets = (word : string) : string[] => {
-  const result = [];
+// Subsets
+
+const subsets = (word: string): string[] => {
+  const result: string[] = [];
   for (let i = 1; i < word.length; i++) {
     result.push(word.substring(i));
   }
-  return result;
-}
 
-console.log(subsets("natalia"));
+  return result;
+};
+
+// console.log(subsets("message"));
+
+// Challenge
+
+const subsetsChallenge = (word: string): string[] => {
+  const wordArray: string[] = word.split("");
+
+  const result = wordArray.map((_, i) => word.substring(i + 1));
+  result.pop();
+
+  return result;
+};
+
+console.log(subsetsChallenge("message"));
